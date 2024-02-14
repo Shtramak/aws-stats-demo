5a36eb2e8b7ea021444f48a4db09ebc98c9898f6
andriikova <andriikova@ext.inditex.com>
2024-02-14 09:52:34 +0200

    add aws starter dependency

--- src/main/java/ua/shtramak/edu/aws/awsstatsapp/properties/AwsStatsProperties.java
+++ src/main/java/ua/shtramak/edu/aws/awsstatsapp/properties/AwsStatsProperties.java
@@ -1,9 +1,11 @@
 package ua.shtramak.edu.aws.awsstatsapp.properties;
 
+import io.awspring.cloud.context.config.annotation.EnableContextInstanceData;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableContextInstanceData
 public class AwsStatsProperties {
     @Value("${ami-id}")
     private String amiId;
